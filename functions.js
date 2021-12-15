4bddc203f2f239408ff87cd708b785739267e508
mounika <mounikamusham12@gmail.com>
2021-12-15 18:20:12 +0530

    functions

---
+++ functions.js
@@ -0,0 +1,27 @@
+function nameOfFunction (){
+
+}  
+console.log(typeof nameOfFunction);
+
+//
+function greet(name){  
+     console.log("hello" +" " + name + " "+ "how are you");
+
+}
+
+greet("MounikaMusham");
+
+//
+
+function add(a,b){
+        return a + b;
+}
+var result=add(10,20);
+console.log("the addition of two numbers is" +" "+result);
+
+//
+function sub(c,d){
+    return c-d;
+}
+var output=sub(5,2);
+console.log("the substraction of 5 and 2 is:" +" "+output);
